5d8e7a2f724be65ca80b4abd1aef7663861c2f23
Eduardo Dutra <emdutra@gmail.com>
2015-07-22 19:39:20 -0300

    queries para trazer dados de tabelas sql

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var sql = require('mssql'); 
+var mssql = require('mssql');
+var sql = require('sql');  
 
 var config = {
     user: 'boletim',
@@ -10,23 +11,107 @@ var config = {
     //     encrypt: true // Use this if you're on Windows Azure
     // }
 }
-
+  
 var testeConfig = JSON.parse('{"user": "boletim", "password": "bolet!m", "server": "mssql2.gear.host", "database": "boletim"}');
 
-var conn = new sql.Connection(testeConfig);
+
+sql.setDialect('mssql');
+var columns = sql.define({
+	name: 'columns',
+	schema: 'information_schema',
+	columns: [
+		{ name: 'table_schema', property: 'tableSchema' },
+		{ name: 'table_name', property: 'tableName' },
+		{ name: 'table_catalog', property: 'tableCatalog' },
+		{ name: 'column_name', property: 'name' },
+		{ name: 'ordinal_position', property: 'ordinalPosition' },
+		{ name: 'data_type', property: 'type' },
+		{ name: 'character_maximum_length', property: 'charLength' },
+		{ name: 'column_default', property: 'defaultValue' },
+		{ name: 'is_nullable', property: 'isNullable' }
+	]
+});
+var tables = sql.define({
+	name: 'tables',
+	schema: 'information_schema',
+	columns: [
+		{ name: 'table_name', property: 'name' },
+		{ name: 'table_schema', property: 'schema' },
+		{ name: 'table_catalog', property: 'catalog' },
+		{ name: 'table_type', property: 'type' }
+	]
+});
+
+var queryTables = tables
+	.select(tables.name.as('name'))
+	.from(tables)
+    .where(tables.schema.equals('dbo'))
+	.and(tables.catalog.equals('boletim'))
+	.and(tables.type.equals('BASE TABLE'))
+    .and(tables.name.notEquals('sysdiagrams'))
+    .toQuery();
+    
+
+
+var conn = new mssql.Connection(testeConfig);
 
 conn.connect(
     function(err) {
 
     var req = conn.request();
-    req.input('rede', 1);
-    req.query('select * from Rede where IdRede = @rede', function(err, recordset) {
+    
+    //var parametros = [1, 2015];
+    
+    queryTables.values.map( function(e, rank) { // magic 
+        req.input(rank+1, e);     
+    });
+    
+
+    req.query(queryTables.text, function(err, recordset) {
     
         console.log(recordset); // return 1
+        
+        console.log('Iniciando a varredura de tabelas\n\n');
+        
+            recordset.forEach(function(table) {
+                
+                
+                
+                var queryColumns = columns
+        			.select(
+        				columns.name.as('name'),
+        				columns.isNullable.as('nullable'),
+        				columns.defaultValue.as('default_value'),
+        				columns.type.as('type')
+        			)
+        			.from(columns)
+        			.where(columns.tableName.equals(table.name))    
+                    .and(columns.tableSchema.equals('dbo'))
+					.and(columns.tableCatalog.equals('boletim'))
+                    .toQuery();
+                 
+                 //console.log(queryColumns);
+                
+                 var reqCol = conn.request();
+                 
+                queryColumns.values.map( function(e, rank) { // magic 
+                    reqCol.input(rank+1, e);     
+                }); 
+                
+                reqCol.query(queryColumns.text, function(err, recordset) {
+                    console.log('Tabela: ' + table.name + '\n');
+                    
+                    console.log(recordset); 
+                });
+                 
+            }, this);
+             
+            conn.close();
+    
     
     });
     
-    conn.close();
+    //
 });
 
 
